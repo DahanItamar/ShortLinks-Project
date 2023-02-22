300af2c3b79a84aa91e3fa4c22eaebbbd3747a3c
DahanItamar <itamardahan1111d@gmail.com>
2023-02-22 22:53:32 +0200

    Add Entry class

---
+++ Models/Entry.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShortLinks.Models
+{
+	public class Entry
+	{
+		[Key]
+		public DateTime EntryDate { get; set; }
+		public string Public_IP_Address { get; set; }
+	}
+}
